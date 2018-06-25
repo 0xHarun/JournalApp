1d170c932de4ad4602ddf78451ca44e16417b772
Harun G. Mbugua <geohar.gm@gmail.com>
2018-06-26 00:14:07 +0300

    test

--- app/src/main/java/com/example/user/myapplication/FirstPage.java
+++ app/src/main/java/com/example/user/myapplication/FirstPage.java
@@ -1,13 +1,57 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FirstPage extends AppCompatActivity {
+
+    private Button mLogOutBtn;
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            if (firebaseAuth.getCurrentUser() == null){
+                startActivity(new Intent(FirstPage.this, MainActivity.class));
+            }
+            }
+        };
+
+        mLogOutBtn = (Button) findViewById(R.id.LogOutBtn);
+
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.signOut();
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
     }
 }
